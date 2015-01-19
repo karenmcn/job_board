a03ae45c64eec46619fc18fc890946162c7210fa
Karen Nelson <karennelson@Karens-MacBook-Air.local>
2015-01-18 16:53:47 -0800

    Trying yet again to get job app to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118210109) do
+ActiveRecord::Schema.define(version: 20150119004923) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "address"
+    t.boolean  "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
